8411fbebd2f086ecd61f7b959642874b40d169be
leah wanjiku <leagm22@gmail.com>
2022-11-29 08:39:56 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1=2;
+let num2=31;
+let num3=29;
+let num4=5;
+let max; 
+let multiply=num1*num2;
+let mod=num3%num4;
+max=Math.max(15,17,20)
+let random;
+random=(Math.floor(Math.random()*10)+1);
